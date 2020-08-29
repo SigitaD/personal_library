de43a0498ad0dfb1426a13741010f75ea26768da
SigitaD <58944306+SigitaD@users.noreply.github.com>
2020-08-29 17:28:17 +0300

    implemented options menu

--- application.py
+++ application.py
@@ -1,19 +1,18 @@
-import os
+import ast
+from tempfile import mkdtemp
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from werkzeug.debug import DebuggedApplication
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import apology, login_required
-from datetime import date
 
+from helpers import apology, login_required
 
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -22,6 +21,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -35,19 +35,20 @@ db = SQL("sqlite:///books.db")
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    #if request.method == "POST":
+    # if request.method == "POST":
     # rodyti pasirinkto list'o knygas 
-    
-    #else:
+
+    # else:
     if request.method == "GET":
         # presents currently reading books
 
         books = db.execute("""SELECT * FROM books WHERE user_id = :id
                             AND started IS NOT NULL AND finished IS NULL """,
-                            id = session["user_id"])
-        
+                           id=session["user_id"])
+
         print(books)
-        return render_template("index.html", books = books)
+        return render_template("index.html", books=books)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -96,6 +97,7 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -132,7 +134,7 @@ def register():
         elif not password == confirmation:
             return apology("passwords do not match", 403)
 
-        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username = username, hash = hashed)
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=hashed)
 
         # Log user in
         session["user_id"] = id
@@ -142,6 +144,7 @@ def register():
 
     else:
         return render_template("register.html")
+
 
 @app.route("/new-book", methods=["GET", "POST"])
 @login_required
@@ -159,53 +162,60 @@ def new_book():
         print('NEW BOOK | genre: ' + str(genre), flush=True)
         notes = request.form.get("notes")
         print('NEW BOOK | notes: ' + str(notes), flush=True)
-        belongingCheck = request.form.get("belongingCheck")
-        print('NEW BOOK | belongingCheck: ' + str(belongingCheck), flush=True)
+        belonging_check = request.form.get("belongingCheck")
+        print('NEW BOOK | belongingCheck: ' + str(belonging_check), flush=True)
         start_date = request.form.get("start_date")
         print('NEW BOOK | start_date: ' + str(start_date), flush=True)
         finish_date = request.form.get("finish_date")
         print('NEW BOOK | finish_date: ' + str(finish_date), flush=True)
         ratings = request.form.get("ratings")
         print('NEW BOOK | ratings: ' + str(ratings), flush=True)
-        dateReadingNow = request.form.get("dateReadingNow")
-        print('NEW BOOK | dateReadingNow: ' + str(dateReadingNow), flush=True)
+        date_reading_now = request.form.get("dateReadingNow")
+        print('NEW BOOK | dateReadingNow: ' + str(date_reading_now), flush=True)
 
+        if not date_reading_now:
+            db.execute(
+                "INSERT INTO books "
+                "(user_id, title, author, pages, isbn, genre, rating, started, finished, owner, notes) "
+                "VALUES "
+                "(:user_id, :title, :author, :pages, :isbn, :genre, :rating, :started, :finished, :owner, :notes)",
+                user_id=session["user_id"],
+                title=title,
+                author=author,
+                pages=page_count,
+                isbn=isbn,
+                genre=genre,
+                rating=ratings,
+                started=start_date,
+                finished=finish_date,
+                owner=belonging_check,
+                notes=notes)
 
-        if not dateReadingNow: 
-            db.execute("INSERT INTO books (user_id, title, author, pages, isbn, genre, rating, started, finished, owner, notes) VALUES (:user_id, :title, :author, :pages, :isbn, :genre, :rating, :started, :finished, :owner, :notes)",
-                user_id = session["user_id"],
-                title = title,
-                author = author,
-                pages = page_count,
-                isbn = isbn,
-                genre = genre,
-                rating = ratings,
-                started = start_date,
-                finished = finish_date,
-                owner = belongingCheck,
-                notes = notes)
-
-            return redirect ("/")
+            return redirect("/")
 
         else:
-            db.execute("INSERT INTO books (user_id, title, author, pages, isbn, genre, rating, started, owner, notes) VALUES (:user_id, :title, :author, :pages, :isbn, :genre, :rating, :started, :owner, :notes)",
-                user_id = session["user_id"],
-                title = title,
-                author = author,
-                pages = page_count,
-                isbn = isbn,
-                genre = genre,
-                rating = ratings,
-                started = dateReadingNow,
-                owner = belongingCheck,
-                notes = notes)
+            db.execute(
+                "INSERT INTO books "
+                "(user_id, title, author, pages, isbn, genre, rating, started, owner, notes) "
+                "VALUES "
+                "(:user_id, :title, :author, :pages, :isbn, :genre, :rating, :started, :owner, :notes)",
+                user_id=session["user_id"],
+                title=title,
+                author=author,
+                pages=page_count,
+                isbn=isbn,
+                genre=genre,
+                rating=ratings,
+                started=date_reading_now,
+                owner=belonging_check,
+                notes=notes)
 
-            return redirect ("/")
+            return redirect("/")
 
     else:
         return render_template("new-book.html")
 
-    
+
 @app.route("/quotes", methods=["GET", "POST"])
 @login_required
 def quotes():
@@ -216,41 +226,41 @@ def quotes():
         quotes = db.execute("""SELECT * FROM quotes
                             JOIN books ON books.book_id = quotes.book_id
                             WHERE quotes.user_id = :user_id""",
-                            user_id = session["user_id"])
+                            user_id=session["user_id"])
 
         # query database for data about books
         books = db.execute("SELECT * FROM books WHERE user_id = :id",
-            id = session["user_id"])
+                           id=session["user_id"])
 
         # if there are no books in database
         if books == 0:
             return apology("You need to add a book first!")
 
-        return render_template("quotes.html", quotes = quotes, books = books)
+        return render_template("quotes.html", quotes=quotes, books=books)
 
     else:
         quote = request.form.get("quote")
         author = request.form.get("author")
-        bookTitle = request.form.get("book_title")
+        book_title = request.form.get("book_title")
 
         if not quote:
             return apology("type your quote!", 404)
-        
+
         if not author:
             return apology("select an author", 404)
 
-        if not bookTitle:
+        if not book_title:
             return apology("select the book ", 404)
 
         books = db.execute("""SELECT book_id FROM books WHERE title = :title AND user_id = :user_id""",
-            title = bookTitle,
-            user_id = session["user_id"])
+                           title=book_title,
+                           user_id=session["user_id"])
 
         # inserting new data into database
         db.execute("INSERT INTO quotes (user_id, book_id, quote) VALUES (:user_id, :book_id, :quote)",
-            user_id = session["user_id"],
-            book_id = books[0]["book_id"],
-            quote = quote)
+                   user_id=session["user_id"],
+                   book_id=books[0]["book_id"],
+                   quote=quote)
 
         return redirect("/quotes")
 
@@ -262,59 +272,88 @@ def book(book_id):
     if request.method == "GET":
         data = db.execute("""SELECT * FROM books
                             WHERE book_id = :book_id
-                            AND user_id = :user_id """,
-                            book_id = book_id,
-                            user_id = session["user_id"])
+                             AND user_id = :user_id """,
+                          book_id=book_id,
+                          user_id=session["user_id"])
         print('BOOK PROFILE | data: ' + str(data), flush=True)
 
         quotes = db.execute("""SELECT * FROM quotes
                             JOIN books ON books.book_id = quotes.book_id
                             WHERE books.book_id = :book_id 
                             AND books.user_id = :user_id """,
-                            book_id = book_id,
-                            user_id = session["user_id"])
+                            book_id=book_id,
+                            user_id=session["user_id"])
 
         lendings = db.execute("""SELECT * FROM lending
                             JOIN books ON books.book_id = lending.book_id
                             WHERE books.book_id = :book_id 
                             AND books.user_id = :user_id """,
-                            book_id = book_id,
-                            user_id = session["user_id"])
-
+                              book_id=book_id,
+                              user_id=session["user_id"])
         # if book in data:
         #     book_id = data[book_id]
         return render_template("book.html", data=data, quotes=quotes, lendings=lendings)
-
-    else:
+    elif request.method == "POST":
         return redirect("/book")
-    
+    elif request.method == "DELETE":
+        dict_str = request.data.decode("UTF-8")
+        data = ast.literal_eval(dict_str)
 
-@app.route("/delete", methods = ["GET", "POST"])
+        book_id = data['bookId']
+
+        db.execute(""" DELETE FROM books WHERE book_id = :book_id""",
+                   book_id=book_id)
+
+        return 0
+
+
+@app.route("/started-reading-book", methods=["POST"])
 @login_required
-def delete():
-    # delete book from database
+def started_reading_book():
     if request.method == "POST":
-        #book = request.form.get("title")
-        #print(book)
-        db.execute("""DELETE FROM books WHERE title = :title AND author = :author AND started = :started AND user_id = :user_id """,
-                    title = request.form.get("title"),
-                    author = request.form.get("author"),
-                    started = request.form.get("started"),
-                    user_id = session["user_id"])
-        return render_template("index.html")
+        book_id = request.form.get("update_book_id")
+        update_start_date = request.form.get("update_start_date")
+
+        db.execute(
+            """UPDATE books
+            SET started = :update_start_date
+            WHERE book_id = :book_id""",
+            update_start_date=update_start_date,
+            book_id=book_id)
+
+        return redirect("/")
 
 
-@app.route("/remove", methods = ["GET", "POST"])
+@app.route("/finished-reading-book", methods=["POST"])
 @login_required
-def remove():
-    #remove book from the list 'reading now'
+def finished_reading_book():
     if request.method == "POST":
-        db.execute("""DELETE started FROM book WHERE title = :title AND author = :author AND started = :started AND user_id = :user_id""",
-                    title = request.form.get("title"),
-                    author = request.form.get("author"),
-                    started = request.form.get("started"),
-                    user_id = session["user_id"])
-        
-        return render_template("index.html")
+        book_id = request.form.get("update_book_id")
+        update_finish_date = request.form.get("update_finish_date")
 
-# app = DebuggedApplication(app, evalex=True)
+        db.execute(
+            """UPDATE books
+            SET finished = :update_finish_date
+            WHERE book_id = :book_id""",
+            update_finish_date=update_finish_date,
+            book_id=book_id)
+
+        return redirect("/")
+
+
+@app.route("/not-reading-book", methods=["POST"])
+@login_required
+def not_reading_book():
+    if request.method == "POST":
+        dict_str = request.data.decode("UTF-8")
+        data = ast.literal_eval(dict_str)
+
+        book_id = data['bookId']
+
+        db.execute(
+            """UPDATE books
+            SET started = NULL
+            WHERE book_id = :book_id""",
+            book_id=book_id)
+
+        return 0
